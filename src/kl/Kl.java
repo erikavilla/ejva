584949a8f757626172a1f170abeb0f63161c2c6c
erikavilla <ejvilla@espol.edu.ec>
2020-11-11 15:11:41 -0500

    kl2

--- src/kl/Kl.java
+++ src/kl/Kl.java
@@ -16,6 +16,8 @@ public class Kl {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("hola mundo");
+        System.out.println("hola mundo2");
     }
     
 }
